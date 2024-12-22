1a945337372c688989e63800576c13a92582fc9e
Esubalew Chekol <esubalewchekol6@gmail.com>
2024-12-23 00:37:08 +0300

    Update bot.py

--- bott/bot.py
+++ bott/bot.py
@@ -3,7 +3,7 @@ from telegram.ext import (
     CommandHandler,
     ContextTypes,
 )
-from os
+import os
 import logging
 from telegram import Update
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
